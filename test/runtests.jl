439097cfaccd8f47ac97f67ef05647a67bc91b7f
Bill <wherrera10@users.noreply.github.com>
2020-10-02 00:19:18 -1000

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,6 +10,9 @@ while true
     println("set to $(n[1])")
     setcolor(aur, n[1] & 0xffffff)
     sleep(0.2)
-    n[1] += 0x25 >> mod(n[2], 3) * 8
+    n[1] += 0xb >> mod(n[2], 3) * 8
     n[2] += 1
+    mod(n[2], 20) == 0 && (n[1] = 0xeeee00)
+    mod(n[2], 40) == 0 && (n[1] = 0xe00ee)
+    mod(n[2], 60) == 0 && (n[1] = 0x00eeee)
 end
