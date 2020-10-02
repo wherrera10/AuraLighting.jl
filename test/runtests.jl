b93791a162cdc13d7aa011461af0809d31552048
Bill <wherrera10@users.noreply.github.com>
2020-10-02 00:11:14 -1000

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,11 +3,13 @@ using Test
 
 lighting = AuraMb()
 
-@test lighting isa AuraMb
+@test lighting isa AuraMbControl
 
-
+const n = [0x707070, 0x3]
 while true
-    setcolor(lighting, 0xffffff)
+    println("set to $(n[1])")
+    setcolor(aur, n[1] & 0xffffff)
     sleep(0.2)
+    n[1] += 0x25 >> mod(n[2], 3) * 8
+    n[2] += 1
 end
-
