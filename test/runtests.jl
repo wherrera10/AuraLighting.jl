74ada788c1d351c45d7c78fa2ea1224175040d6a
Bill <wherrera10@users.noreply.github.com>
2020-10-02 12:22:43 -1000

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,20 +1,29 @@
 using AuraLighting
 using Test
 
-lighting = AuraMb()
+lighting = AuraMb(1, isservice=true)
 
-@test lighting isa AuraMbControl
+@testset "Direct control" begin
 
-const n = [0x707070, 0x3]
-while true
-    println("set to $(n[1])")
-    setcolor(aur, n[1] & 0xffffff)
-    sleep(0.2)
-    n[1] += 0xb >> mod(n[2], 3) * 8
-    n[2] += 1
-    mod(n[2], 20) == 0 && (n[1] = 0xeeee00)
-    mod(n[2], 40) == 0 && (n[1] = 0xe00ee)
-    mod(n[2], 60) == 0 && (n[1] = 0x00eeee)
-end
+    @test lighting isa AuraMbControl
+    
+    const n = [0x707070, 0x3]
+    while true
+        println("set to $(n[1])")
+        setcolor(aur, n[1] & 0xffffff)
+        sleep(0.2)
+        n[1] += 0xb >> mod(n[2], 3) * 8
+        n[2] += 1
+        mod(n[2], 20) == 0 && (n[1] = 0xeeee00)
+        mod(n[2], 40) == 0 && (n[1] = 0xe00ee)
+        mod(n[2], 60) == 0 && (n[1] = 0x00eeee)
+    end
+
+end #testset
+    
+@testset "Client-server control" begin
+
+
+end #testset
 
 AuraLighting.setautomode(aur, to_automatic=true)
