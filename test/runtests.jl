b8c29b4b1c14ba20e531a862ba8b4bb585e23cd3
Bill <wherrera10@users.noreply.github.com>
2020-10-03 11:57:18 -1000

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,29 +1,83 @@
 using AuraLighting
 using Test
 
-aur = AuraMbControl(1, isservice=true)
-
 @testset "Direct control" begin
 
+    aur = AuraMbControl()
     @test aur isa AuraMbControl
-    
-    const n = [0x707070, 0x3]
-    while n[2] < 100
-        println("set to $(n[1])")
-        setcolor(aur, n[1] & 0xffffff)
+
+    setmode(aur, 1)
+
+    println("white")
+    setcolor(aur, 0xff, 0xff, 0xff)
+    sleep(0.5)
+    r, g, b = AuraLighting.getcolor(aur)
+    @test r == 0xff
+    @test g == 0xff
+    @test b == 0xff
+
+    println("black")
+    setcolor(aur, 0x0, 0x0, 0x0)
+    sleep(0.1)
+    r, g, b = AuraLighting.getcolor(aur)
+    @test r == 0
+    @test g == 0
+    @test b == 0
+
+    setcolor(aur, 0xff, 0x0, 0x0)
+    sleep(0.1)
+    r, g, b = AuraLighting.getcolor(aur)
+    @test r == 0xff
+    @test g == 0
+    @test b == 0
+
+    setcolor(aur, 0x0, 0xff, 0x0)
+    sleep(0.1)
+
+    r, g, b = getcolor(aur)
+    @test r == 0
+    @test g == 0xff
+    @test b == 0
+
+    setcolor(aur, 0x0, 0x0, 0xff)
+    sleep(0.1)
+
+    r, g, b = getcolor(aur)
+    @test r == 0
+    @test g == 0
+    @test b == 0xff
+
+    n = 0xffffff
+    ar, ag, ab = collect(0:255), collect(0:255), collect(0:255)
+    for i in 1:10
+        n &= 0xffffff
+        println("Set color to $n")
+        setcolor(aur, n)
         sleep(0.2)
-        n[1] += 0xb >> mod(n[2], 3) * 8
-        n[2] += 1
-        mod(n[2], 20) == 0 && (n[1] = 0xeeee00)
-        mod(n[2], 40) == 0 && (n[1] = 0xe00ee)
-        mod(n[2], 60) == 0 && (n[1] = 0x00eeee)
+        r, g, b = getcolor(aur)
+        @test (r << 16) | (g << 8) | b == n
+        r, g, b = rand(ar), rand(ag), rand(ab)
+        n = (r << 16) | (g << 8) | b
     end
 
 end #testset
-    
+
 @testset "Client-server control" begin
 
+    aur = AuraMbControl()
+    @test aur isa AuraMbControl
+
+
+    @test 1 == 1
 
 end #testset
 
-AuraLighting.setautomode(aur, to_automatic=true)
+@testset "Restore to auto mode" begin
+
+    aur = AuraMbControl()
+    @test aur isa AuraMbControl
+
+    setmode(aur, 0)
+
+end #testset
+
