b19ac2b36f16f4c6ba91d015a7ddc6e4ca4a8433
Bill <wherrera10@users.noreply.github.com>
2020-10-03 23:56:51 -1000

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 using AuraLighting
-using Test
+using Test, Random
 
 @testset "Direct control" begin
 
@@ -10,7 +10,7 @@ using Test
 
     println("white")
     setcolor(aur, 0xff, 0xff, 0xff)
-    sleep(0.5)
+    sleep(0.8)
     r, g, b = AuraLighting.getcolor(aur)
     @test r == 0xff
     @test g == 0xff
@@ -18,46 +18,46 @@ using Test
 
     println("black")
     setcolor(aur, 0x0, 0x0, 0x0)
-    sleep(0.1)
+    sleep(0.8)
     r, g, b = AuraLighting.getcolor(aur)
     @test r == 0
     @test g == 0
     @test b == 0
 
+    println("red")
     setcolor(aur, 0xff, 0x0, 0x0)
-    sleep(0.1)
+    sleep(0.8)
     r, g, b = AuraLighting.getcolor(aur)
     @test r == 0xff
     @test g == 0
     @test b == 0
 
+    println("green")
     setcolor(aur, 0x0, 0xff, 0x0)
-    sleep(0.1)
-
+    sleep(0.8)
     r, g, b = getcolor(aur)
     @test r == 0
     @test g == 0xff
     @test b == 0
 
+    println("blue")
     setcolor(aur, 0x0, 0x0, 0xff)
-    sleep(0.1)
-
+    sleep(0.8)
     r, g, b = getcolor(aur)
     @test r == 0
     @test g == 0
     @test b == 0xff
 
-    n = 0xffffff
-    ar, ag, ab = collect(0:255), collect(0:255), collect(0:255)
-    for i in 1:10
-        n &= 0xffffff
+    picks = collect(0x0:0xff)
+    for i in 1:5
+        sleep(0.8)
+        c = view(shuffle!(picks),1:3)
+        n = rgbtoi(c[1], c[2], c[3])
         println("Set color to $n")
         setcolor(aur, n)
-        sleep(0.2)
-        r, g, b = getcolor(aur)
-        @test (r << 16) | (g << 8) | b == n
-        r, g, b = rand(ar), rand(ag), rand(ab)
-        n = (r << 16) | (g << 8) | b
+        sleep(0.8)
+        r2, g2, b2 = getcolor(aur)
+        @test rgbtoi(c[1], c[2], c[3]) == n
     end
 
 end #testset
@@ -65,10 +65,20 @@ end #testset
 @testset "Client-server control" begin
 
     aur = AuraMbControl()
+    sleep(0.8)
     @test aur isa AuraMbControl
-
-
-    @test 1 == 1
+    startserver(aur)
+    sleep(1)
+    cli = AuraMBControlClient(1)
+    @test cli isa AuraMBControlClient
+    @test iscorrectcontroller(cli)
+    println("Client request yellow")
+    setcolor(cli, 0xff00ff)
+    sleep(3)
+    r, g, b = getcolor(cli)
+    @test r == 0xff
+    @test g == 0x0
+    @test b == 0xff
 
 end #testset
 
@@ -76,8 +86,6 @@ end #testset
 
     aur = AuraMbControl()
     @test aur isa AuraMbControl
-
     setmode(aur, 0)
 
 end #testset
-
