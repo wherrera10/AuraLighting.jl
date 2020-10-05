1497d1a6217898b5537cfbf78aadbb2707bf9052
Bill <wherrera10@users.noreply.github.com>
2020-10-04 15:24:58 -1000

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,93 +1,117 @@
 using AuraLighting
 using Test, Random
 
-GC.enable(false)
+if sizeof(C_NULL) == 4 # 32-bit mode can use the 32-bit DLL AURA_SDK.dll
 
-@testset "Direct control" begin
+    GC.enable(false)
 
-    aur = AuraMbControl()
-    @test aur isa AuraMbControl
+    @testset "Direct control" begin
 
-    setmode(aur, 1)
-    sleep(0.5)
+        aur = AuraMbControl()
+        @test aur isa AuraMbControl
 
-    println("white")
-    setcolor(aur, 0xff, 0xff, 0xff)
-    sleep(0.8)
-    r, b, g = AuraLighting.getcolor(aur)
-    @test r == 0xff
-    @test b == 0xff
-    @test g == 0xff
+        setmode(aur, 1)
+        sleep(0.5)
 
-    println("black")
-    setcolor(aur, 0x0, 0x0, 0x0)
-    sleep(0.8)
-    r, b, g = AuraLighting.getcolor(aur)
-    @test r == 0
-    @test b == 0
-    @test g == 0
-
-    println("red")
-    setcolor(aur, 0xff, 0x0, 0x0)
-    sleep(0.8)
-    r, b, g = AuraLighting.getcolor(aur)
-    @test r == 0xff
-    @test b == 0
-    @test g == 0
-
-    println("blue")
-    setcolor(aur, 0x0, 0xff, 0x0)
-    sleep(0.8)
-    r, b, g = getcolor(aur)
-    @test r == 0
-    @test b == 0xff
-    @test g == 0
-
-    println("green")
-    setcolor(aur, 0x0, 0x0, 0xff)
-    sleep(0.8)
-    r, b, g = getcolor(aur)
-    @test r == 0
-    @test b == 0
-    @test g == 0xff
-
-    picks = collect(0x0:0xff)
-    for i in 1:5
+        println("white")
+        setcolor(aur, 0xff, 0xff, 0xff)
         sleep(0.8)
-        c = view(shuffle!(picks),1:3)
-        n = UInt(rbgtoi(c[1], c[2], c[3]))
-        println("Set color to $n")
-        setcolor(aur, n)
+        r, b, g = AuraLighting.getcolor(aur)
+        @test r == 0xff
+        @test b == 0xff
+        @test g == 0xff
+
+        println("black")
+        setcolor(aur, 0x0, 0x0, 0x0)
         sleep(0.8)
-        r2, g2, b2 = getcolor(aur)
-        @test rbgtoi(c[1], c[2], c[3]) == n
-    end
+        r, b, g = AuraLighting.getcolor(aur)
+        @test r == 0
+        @test b == 0
+        @test g == 0
 
-end #testset
+        println("red")
+        setcolor(aur, 0xff, 0x0, 0x0)
+        sleep(0.8)
+        r, b, g = AuraLighting.getcolor(aur)
+        @test r == 0xff
+        @test b == 0
+        @test g == 0
 
-@testset "Client-server control" begin
+        println("blue")
+        setcolor(aur, 0x0, 0xff, 0x0)
+        sleep(0.8)
+        r, b, g = getcolor(aur)
+        @test r == 0
+        @test b == 0xff
+        @test g == 0
 
-    aur = AuraMbControl()
-    startserver(aur)
-    sleep(1)
+        println("green")
+        setcolor(aur, 0x0, 0x0, 0xff)
+        sleep(0.8)
+        r, b, g = getcolor(aur)
+        @test r == 0
+        @test b == 0
+        @test g == 0xff
 
-    cli = AuraMBControlClient(1)
-    @test cli isa AuraMBControlClient
-    @test iscorrectcontroller(cli)
+        picks = collect(0x0:0xff)
+        for i in 1:5
+            sleep(0.8)
+            c = view(shuffle!(picks),1:3)
+            n = UInt(rbgtoi(c[1], c[2], c[3]))
+            println("Set color to $n")
+            setcolor(aur, n)
+            sleep(0.8)
+            r2, g2, b2 = getcolor(aur)
+            @test rbgtoi(c[1], c[2], c[3]) == n
+        end
 
-    println("Client request yellow")
-    setcolor(cli, 0xff00ff)
-    sleep(3)
-    r, g, b = getcolor(cli)
-    @test r == 0xff
-    @test g == 0x0
-    @test b == 0xff
+    end #testset
 
-end #testset
+    @testset "Client-server control" begin
 
-@testset "Restore to auto mode" begin
-    aur = AuraMbControl()
-    @test aur isa AuraMbControl
-    setmode(aur, 0)
+        aur = AuraMbControl()
+        startserver(aur)
+        sleep(200)
 
-end #testset
+        cli = AuraMBControlClient(1)
+        @test cli isa AuraMBControlClient
+        @test iscorrectcontroller(cli)
+
+        println("Client request yellow")
+        setcolor(cli, 0xff00ff)
+        sleep(3)
+        r, g, b = getcolor(cli)
+        @test r == 0xff
+        @test g == 0x0
+        @test b == 0xff
+
+    end #testset
+
+    @testset "Restore to auto mode" begin
+        aur = AuraMbControl()
+        @test aur isa AuraMbControl
+        setmode(aur, 0)
+
+    end #testset
+
+elseif sizeof(C_NULL) == 8  # 64-bit mode
+
+    @testset "64-bit client doing Client-server control" begin
+
+        cli = AuraMBControlClient()
+        @test cli isa AuraMBControlClient
+        @test iscorrectcontroller(cli)
+
+        println("Client request yellow")
+        setcolor(cli, 0xff00ff)
+        sleep(3)
+        r, g, b = getcolor(cli)
+        @test r == 0xff
+        @test g == 0x0
+        @test b == 0xff
+
+        @test sendexit(aur)
+
+    end #testset
+
+end
